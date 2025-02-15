5bd4ad70787bd60b06f502334303a20dded953ed
Saurabh Mishra <saurabh.mishra45k@gmail.com>
2025-02-15 23:15:55 +0530

    add leetcode_3452_sum_of_good_numbers.py

---
+++ Biweekly-contests/contest-150-2024-02-25/leetcode_3452_sum_of_good_numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfGoodNumbers(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            if (i - k >= 0 and nums[i] <= nums[i-k]):
+                continue
+            elif (i+k <len(nums) and nums[i] <= nums[i+k]):
+                continue
+            else:
+                res += nums[i]
+        return res
